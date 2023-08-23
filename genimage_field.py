d4db7ca23a1f3bcd084163233bb91ba21f0ad408
Mauro Rigo <mrigo@login1.mgmt>
2023-08-24 00:18:28 +0200

    Uploaded 625 improved

---
+++ genimage_field.py
@@ -0,0 +1,73 @@
+from nbodykit.lab import BigFileMesh
+import argparse
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib.colors import LogNorm
+
+parser = argparse.ArgumentParser()
+parser.add_argument('path', type=str, help='Path of the field.')
+parser.add_argument('--BoxSize', type=float, default=205., help='Size of simulation box.')
+# NO UPSAMPLING SUPPORTED FOR THE TIME BEING
+args = parser.parse_args()
+
+try:
+    Nmesh = int(args.path.split('Nmesh')[1].split('_')[0])
+except:
+    raise Exception("Something wrong with path.")
+
+fout = f'FPMod{Nmesh}improved.png'
+plane = 'XY'
+offset = 0.
+slicew = 20.
+scale = 'log'
+cmap = 'viridis'
+min_overdensity = 0.5
+max_overdensity = 50.0
+
+print("Importing data...")
+
+bfm = BigFileMesh(args.path, 'Field')
+
+print("Done. Generating overdensity...")
+
+overdensity = bfm.preview()
+start = int(offset / args.BoxSize * Nmesh)
+end = start + int(slicew / args.BoxSize * Nmesh) + 1
+indices = [slice(Nmesh)]*3
+axisdict = {'XY': 2, 'XZ': 1, 'YZ': 0}
+axnum = axisdict[plane]
+indices[axnum] = slice(start, end)
+indices = tuple(indices)
+overdensity = overdensity[indices].sum(axis=axnum)/(end-start) # 2D overdensity (IDK WHY NORMLIZATION LIKE THAT)
+overdensity = np.transpose(overdensity)
+#print(np.max(overdensity), np.min(overdensity), np.mean(overdensity))
+
+print("Overdensity generated, creating figure...")
+
+fig = plt.figure()
+ax1 = fig.add_subplot(111)
+
+ax1.set_xlim([0, args.BoxSize])  #set the range for the x-axis
+ax1.set_ylim([0, args.BoxSize])  #set the range for the y-axis
+
+ax1.set_xlabel(r'$h^{-1}{\rm Mpc}$',fontsize=10)  #x-axis label
+ax1.set_ylabel(r'$h^{-1}{\rm Mpc}$',fontsize=10)  #y-axis label
+
+if min_overdensity==None:  min_overdensity = np.min(overdensity)
+if max_overdensity==None:  max_overdensity = np.max(overdensity)
+
+overdensity[np.where(overdensity<min_overdensity)] = min_overdensity
+
+if scale=='linear':
+      cax = ax1.imshow(overdensity, cmap=plt.get_cmap(cmap), origin='lower',
+                       extent=[0, args.BoxSize, 0, args.BoxSize], interpolation='bicubic',
+                       vmin=min_overdensity, vmax=max_overdensity)
+else:
+      cax = ax1.imshow(overdensity, cmap=plt.get_cmap(cmap), origin='lower',
+                       extent=[0, args.BoxSize, 0, args.BoxSize], interpolation='bicubic',
+                       norm = LogNorm(vmin=min_overdensity, vmax=max_overdensity))
+
+cbar = fig.colorbar(cax)
+cbar.set_label(r"$\rho/\bar{\rho}$",fontsize=10)
+plt.savefig(fout, bbox_inches='tight')
+plt.close(fig)
